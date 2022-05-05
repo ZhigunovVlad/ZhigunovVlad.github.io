95d91b973fa6e02bf0a69e86c6f29bde49c1af9a
GLEEEBUS <91493478+GLEEEBUS@users.noreply.github.com>
2022-05-05 23:11:06 +0300

    Отмена последнего изменения

--- cafe.js
+++ cafe.js
@@ -168,7 +168,7 @@ var Cafe = {
                 is_visible: !!Cafe.canPay,
                 text: 'VIEW ORDER',
                 color: '#31b545'
-            }).showProgress();
+            }).hideProgress();
         }
     },
     updateTotalPrice: function() {
