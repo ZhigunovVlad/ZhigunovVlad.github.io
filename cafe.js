9081e85f91bb9776d70437dc35aa598bd8464c2e
Vladislav <69080027+ZhigunovVlad@users.noreply.github.com>
2022-05-08 13:09:11 +0300

    Тест в тупую

--- cafe.js
+++ cafe.js
@@ -276,8 +276,8 @@ var Cafe = {
             success: function(result) {
                 onCallback && onCallback(result);
             },
-            error: function(xhr) {
-                onCallback && onCallback({error: 'Server error'});
+            error: function(result) {
+                onCallback && onCallback(result);
             }
         });
     }
